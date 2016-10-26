3814b59f1cccccdb6e8adfc0a5eed80ae5b29ff9
Karthikeyan B <22561@Hexaware.com>
2016-10-26 14:36:44 +0530

    Table Total Added

--- MVP/controllers/requisitionSearchController.js
+++ MVP/controllers/requisitionSearchController.js
@@ -1,5 +1,10 @@
 app.controller('requisitionSearchController', ['$scope', 'Factory', function($scope, Factory) {
     var promise = Factory.getRequisitionSearchResults();
+	  $scope.ind = '';
+    $scope.cli = '';
+    $scope.name = '';
+    $scope.data = [];
+	
     promise.then(function(response){
             //console.log('hi');
             var result = response.data.requisition;
