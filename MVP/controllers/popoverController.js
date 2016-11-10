ff05a8b6cab8c594f50e5a7f0caddc67e9babbf9
Sujeet Ghanvat <34439@hexaware.com>
2016-11-10 18:05:52 +0530

    Popover Controller

---
+++ MVP/controllers/popoverController.js
@@ -0,0 +1,59 @@
+app.controller('popoverController', ['$scope', '$timeout', function ($scope, $timeout) {
+    $scope.cancel = function () {
+      $scope.isOpen = false;
+    };
+
+    $scope.ReqUrl = "partial/_RequisitionPipeline.html";
+    $scope.CanUrl = "partial/_CandidatePipeline.html";
+
+  var list = {
+        'reqPipeline' : { 'status' : 1, 'type' : 'req', 'url' : 'RequisitionPipeline'},
+        'reqHistory'  : { 'status' : 0, 'type' : 'req', 'url' : 'RequisitionHistory'},
+        'reqActivity' : { 'status' : 0, 'type' : 'req', 'url' : 'RequisitionActivity'},
+        'canPipeline' : { 'status' : 1, 'type' : 'can', 'url' : 'CandidatePipeline'},
+        'canHistory'  : { 'status' : 0, 'type' : 'can', 'url' : 'CandidateHistory'},
+        'canSource'   : { 'status' : 0, 'type' : 'can', 'url' : 'CandidateSource'}
+    }
+
+  $scope.initial = function() {
+      $timeout(function () { initialSetup(); }, 10);
+  }
+
+  var initialSetup = function() {
+      var el = '';
+    angular.forEach(list , function (val, key) {
+        if (val.status == 1) {
+            el = $('#' + key);
+            el.prop('disabled', true);
+            el.prop('checked', true);
+        }
+    })
+  }
+
+     $scope.open = function(type) {
+        var el = $(event.target);
+        var activeEl = '';
+
+        angular.forEach(list, function(val, key) {
+            if (val.type == type) {
+                if (val.status == 1) {
+                    activeEl = $('#' + key);
+                    val.status = 0;
+                    activeEl.prop('disabled', false);
+                    activeEl.prop('checked', false);
+                }
+                if (key == el.attr('id')) {
+                    var str = 'partial/_' + val.url + '.html';
+                    if (type == 'req') $scope.ReqUrl = str;
+                    if (type == 'can') $scope.CanUrl = str;
+
+                    el.prop('disabled', true);
+                    val.status = 1;
+                }
+            }
+
+
+        });
+    }
+
+}]);
