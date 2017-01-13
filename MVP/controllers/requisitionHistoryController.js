f4f98ce74b680984636e434177a066f743ca945f
SomeOne <sujeetgha@gmail.com>
2017-01-13 17:14:45 +0530

    graph bug fixes.

--- MVP/controllers/requisitionHistoryController.js
+++ MVP/controllers/requisitionHistoryController.js
@@ -4,26 +4,42 @@ app.controller('requisitionHistoryController', ['$scope', 'Factory', 'sharedProp
     var deeplinkURL = '';
     var graphName = 'RequisitionHistory';
     var deeplinkURL = '';
+    var graphVar = {};
     $scope.selectedEngagment = null;
     $scope.selectedButton = 'company';
     $scope.selectedYear = '2016';
     var selectedQuater = 'Q4';
 
     function setInitialValues() {
-       $('div[ng-controller="requisitionHistoryController"] a.active').removeClass('active');
+        $('div[ng-controller="requisitionHistoryController"] a.active').removeClass('active');
         $scope.selectedButton == 'mygraph' ? $('#myReqsRH').addClass('active') : $('#clientReqsRH').addClass('active');
         $('div#rhqcdiv a:contains("' + selectedQuater + '")').addClass('active');
     }
     setInitialValues();
     if (angular.isDefined($rootScope.graph[graphName])) {
         var a = $rootScope.graph[graphName];
-        $scope.selectedButton = a.GraphType ? a.GraphType : 'company';
-        $scope.selectedEngagment = a.Engagement;
-        if (a.QuaterYear) {
-            $scope.selectedYear = a.QuaterYear.slice(0, 4);
-            selectedQuater = a.QuaterYear.slice(4, 6);
+        var d = '';
+        if (a.firstTime == true) {
+            if (a.loadedFromBackend == true) {
+                d = a.data;
+            }
+            else {
+                //use default values.
+            }
+            $rootScope.graph[graphName].firstTime = false;
         }
-        setInitialValues();
+        else {
+            d = a.mdata;
+        }
+        if (angular.isObject(d)) {
+            $scope.selectedButton = d.GraphType ? d.GraphType : 'company';
+            $scope.selectedEngagment = d.Engagement;
+            if (d.QuaterYear) {
+                $scope.selectedYear = d.QuaterYear.slice(0, 4);
+                selectedQuater = d.QuaterYear.slice(4, 6);
+            }
+            setInitialValues();
+        }
     }
     var quaterYear = $scope.selectedYear + selectedQuater;
 
@@ -66,35 +82,48 @@ app.controller('requisitionHistoryController', ['$scope', 'Factory', 'sharedProp
         //requisitonGoalStackBarChart(graphName, $scope.selectedButton);
     function requisitonGoalStackBarChart() {
         var companyId = commonFunctions.getCompanyId($scope.allEngagments, $scope.selectedEngagment);
-        var promise = Factory.getChart(graphName, $scope.selectedButton, $scope.selectedEngagment, companyId, quaterYear);
-        promise.then(function resolved(response) {
-            if (Object.keys(response.data.graphDetails.data).length > 0) {
-                deeplinkURL = response.data.graphDetails.deepLinkURI;
-                datas.push(JSON.parse("[" + response.data.graphDetails.data.Fill + "]"));
-                datas.push(JSON.parse("[" + response.data.graphDetails.data.Cancel + "]"));
-                datas.push(JSON.parse("[" + response.data.graphDetails.data.Hold + "]"));
-                datas.push(JSON.parse("[" + response.data.graphDetails.data.Open + "]"));
-                $scope.series = response.data.graphDetails.series
-                $scope.labels = response.data.graphDetails.lables;
-                $scope.data = datas
-                $scope.type = 'StackedBar';
-                $scope.options = {
-                    scales: {
-                        xAxes: [{
-                            stacked: true
+        var v = {
+            'GraphType': $scope.selectedButton
+            , 'Engagement': $scope.selectedEngagment
+            , 'companyId': companyId
+            , 'QuaterYear': quaterYear
+        }
+        if (angular.equals(graphVar, v)) {
+            //do nothing
+        }
+        else {
+            graphVar = Object.assign({}, v);
+            $rootScope.graph[graphName].mdata = Object.assign({}, v);
+            var promise = Factory.getChart(graphName, $scope.selectedButton, $scope.selectedEngagment, companyId, quaterYear);
+            promise.then(function resolved(response) {
+                if (response.data.graphDetails && Object.keys(response.data.graphDetails.data).length > 0) {
+                    deeplinkURL = response.data.graphDetails.deepLinkURI;
+                    datas.push(JSON.parse("[" + response.data.graphDetails.data.Fill + "]"));
+                    datas.push(JSON.parse("[" + response.data.graphDetails.data.Cancel + "]"));
+                    datas.push(JSON.parse("[" + response.data.graphDetails.data.Hold + "]"));
+                    datas.push(JSON.parse("[" + response.data.graphDetails.data.Open + "]"));
+                    $scope.series = response.data.graphDetails.series
+                    $scope.labels = response.data.graphDetails.lables;
+                    $scope.data = datas
+                    $scope.type = 'StackedBar';
+                    $scope.options = {
+                        scales: {
+                            xAxes: [{
+                                stacked: true
                          }]
-                        , yAxes: [{
-                            stacked: true
+                            , yAxes: [{
+                                stacked: true
         }]
-                    }
-                };
-            }
-            else {
-                $scope.data = [];
-            }
-        }, function rejected(response) {
-            commonFunctions.error('Failed to load : ' + response.status + ': ' + response.statusText);
-        })
+                        }
+                    };
+                }
+                else {
+                    $scope.data = [];
+                }
+            }, function rejected(response) {
+                commonFunctions.error('Failed to load : ' + response.status + ': ' + response.statusText);
+            })
+        }
     };
     /*    $scope.onClick = function (points, evt) {
             console.log('hello' + deeplinkURL); // 0 -> Series A, 1 -> Series B
