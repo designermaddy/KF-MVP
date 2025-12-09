aa71cca6289a1997cf9cde382d94351e2e2bc1e8
designermaddy <designermaddy@gmail.com>
2025-12-09 08:38:05 +0530

    Delete MVP directory

--- MVP/controllers/engagementOverviewController.js
+++
@@ -1,9 +0,0 @@
-app.controller('engagementOverviewController', ['$scope','Factory',  function ($scope, Factory) {
- $scope.chart =    {
-     name: 'Chart 1',
-     type: 'Doughnut',
-     labels: ['EVSC', 'ISB'],
-     data: [13, 44]
-   };
-   $scope.options = {legend: {display: true}};
-  }]);
--- MVP/controllers/requisitionDetailsTabController.js
+++
@@ -1,6 +0,0 @@
-app.controller('requisitionDetailsTabController', ['$scope', 'Factory', 'commonFunctions', '$sce', 'config', 'sharedProperties','$timeout', '$location', function ($scope, Factory, commonFunctions, $sce, config, sharedProperties, $timeout, $location) {
-    var url = $location.url();
-    var a = url.split('/');
-    $scope.activetab = parseInt(a[a.length - 1]);
-
-}]);
